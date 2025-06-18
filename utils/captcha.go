e1655fa477f8c9dc0784e62cbc8e477e40687c2e
Nanaloveyuki <136328617+Nanaloveyuki@users.noreply.github.com>
2025-06-18 08:01:48 +0800

    :wastebasket: 清理 [spage/models](./spage/models) 下的英文注释

--- utils/captcha.go
+++ utils/captcha.go
@@ -2,6 +2,7 @@ package utils
 
 import (
 	"fmt"
+
 	"github.com/LiteyukiStudio/spage/constants"
 	"github.com/go-resty/resty/v2"
 )
@@ -17,7 +18,6 @@ type CaptchaConfig struct {
 }
 
 // VerifyCaptcha 根据提供的配置和令牌验证验证码
-// Verify captcha based on provided configuration and token
 func (captchaType) VerifyCaptcha(restyClient *resty.Client, captchaConfig *CaptchaConfig, captchaToken string) (bool, error) {
 	switch captchaConfig.Type {
 	case constants.CaptchaTypeDisable:
